57f2482e28e05a15b6bccc40e3dca2543ed949bd
ksr12345 <kaustubh.raut@somaiya.edu>
2024-07-17 11:15:23 +0530

    Added: Line comparison wrt start and end points

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,11 +1,38 @@
 public class LineComparison {
-    public static void main(String[] args) {
-        double len = calculateLength(2, 3, 5, 7);
-        System.out.println("Length of the line segment: " + len);
+    private final double x1, y1;
+    private final double x2, y2;
+
+    public LineComparison(double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
     }
 
-    public static double calculateLength(double x1, double y1, double x2, double y2) {
-        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        LineComparison other = (LineComparison) obj;
+        return Double.compare(other.x1, x1) == 0 &&
+                Double.compare(other.y1, y1) == 0 &&
+                Double.compare(other.x2, x2) == 0 &&
+                Double.compare(other.y2, y2) == 0;
     }
+
+    public static void main(String[] args) {
+        LineComparison line1 = new LineComparison(2, 3, 5, 7);
+        LineComparison line2 = new LineComparison(2, 3, 5, 7);
+        LineComparison line3 = new LineComparison(1, 2, 4, 6);
+
+        // Testing equality
+        System.out.println("line1 equals line2: " + line1.equals(line2));
+        System.out.println("line1 equals line3: " + line1.equals(line3));
+    }
+
 
 }
