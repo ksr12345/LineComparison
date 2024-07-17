6233ecd90eb0cc17d1d6479b317e8b8788415bc4
ksr12345 <kaustubh.raut@somaiya.edu>
2024-07-17 11:09:33 +0530

    Added: Length Calculation

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,5 +1,11 @@
 public class LineComparison {
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison");
+        double len = calculateLength(2, 3, 5, 7);
+        System.out.println("Length of the line segment: " + len);
     }
+
+    public static double calculateLength(double x1, double y1, double x2, double y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+
 }
